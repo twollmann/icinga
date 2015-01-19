e978d483e0707d27a4542fe658386c60fdb7d8b6
Tobias Wollmann <tobias.wollmann@gmail.com>
2015-01-19 18:01:24 +0000

    finalized cpu check script.

--- check_cpu_by_ssh.rb
+++ check_cpu_by_ssh.rb
@@ -9,6 +9,24 @@
 require 'net/ssh'
 require 'optparse'
 
+# Method to generate the performance data and exit the script
+def printPerfData (utilization, warning, critical, type)
+  # 
+  if utilization < warning && utilization < critical
+    print "CPU OK - #{type} utilization: #{utilization.round(2)}\n"
+    exit 0
+  elsif utilization >= warning && utilization < critical
+    print "CPU WARNING - #{type} utilization: #{utilization.round(2)}\n"
+    exit 1
+  elsif utilization > warning && utilization >= critical
+    print "CPU CRITICAL - #{type} utilization: #{utilization.round(2)}\n"
+    exit 2
+  else
+    print "CPU UNKNOWN\n"
+    exit 3
+  end
+end
+
 #
 # Main
 #
@@ -24,7 +42,10 @@ optparse = OptionParser.new do |opts|
     options[:username] = user
   end
   opts.on("-d", "--delay delay", "Delay betwen two checks.") do |delay|
-    options[:delay] =  delay
+    options[:delay] =  delay.to_i
+  end
+  opts.on("-m", "--mode mode", "Specifies the monitoring mode.") do |mode|
+    options[:mode] =  mode.to_i
   end
   opts.on("-w", "--warning warning", "Warning threshold.") do |warn|
     options[:warning] =  warn
@@ -37,9 +58,12 @@ end.parse!
 # Get the command line parameters and assign it to variables
 hostname   = options[:hostname]
 username   = options[:username]
+delay      = options[:delay]
+mode       = options[:mode]
 warning    = options[:warning]
 critical   = options[:critical]
 
+# Verifies the given parameters
 if hostname == nil
   puts "The hostname is a neccessary parameter."
   exit 3
@@ -48,17 +72,19 @@ elsif username == nil
   exit 3
 elsif delay == nil
   delay = 2
+elsif mode == nil
+  mode = 0
 end
 
 # Get information about the cpu usage
-cpu_info_1 = Net::SSH.start(hostname, username) do |ssh|
-  ssh.exec!("cat /proc/stat | grep -i '^cpu  '").split
-end
-
-sleep(delay.sec)
-
-cpu_info_2 = Net::SSH.start(hostname, username) do |ssh|
-  ssh.exec!("cat /proc/stat | grep -i '^cpu  '").split
+begin
+  connection = Net::SSH.start(hostname, username)
+  cpu_info_1 = connection.exec!("cat /proc/stat | grep -i '^cpu  '").split
+  sleep(delay)
+  cpu_info_2 = connection.exec!("cat /proc/stat | grep -i '^cpu  '").split
+rescue
+  print "CPU UNKNOWN - #{$!}\n"
+  exit 3
 end
 
 # - user: normal processes executing in user mode
@@ -71,12 +97,23 @@ cpu_user   = cpu_info_2.at(1).to_i - cpu_info_1.at(1).to_i
 cpu_nice   = cpu_info_2.at(2).to_i - cpu_info_1.at(2).to_i
 cpu_system = cpu_info_2.at(3).to_i - cpu_info_1.at(3).to_i
 cpu_idle   = cpu_info_2.at(4).to_i - cpu_info_1.at(4).to_i
+cpu_time   = cpu_user + cpu_nice + cpu_system + cpu_idle
 
-cpu_total  = cpu_user + cpu_nice + cpu_system + cpu_idle
-
-puts cpu_total.to_s
-
-puts cpu_user.to_s
-puts cpu_nice.to_s
-puts cpu_system.to_s
-puts cpu_idle.to_s
+# Generate performance data output regarding the selected monitoring mode.
+if mode == 0
+  # Total CPU utilization
+  perf_total = (100.to_f / cpu_time.to_f) * (cpu_user + cpu_nice + cpu_system)
+  printPerfData(perf_total, warning, critical, "Total")
+elsif mode == 1
+  # Cpu utilization of user processes
+  perf_user   = (100.to_f / cpu_time.to_f) * cpu_user.to_f
+  printPerfData(perf_user, warning, critical, "User")
+elsif mode == 2
+  # CPU utilization of nice processes
+  perf_nice   = (100.to_f / cpu_time.to_f) * cpu_nice.to_f
+  printPerfData(perf_nice, warning, critical, "Nice")
+elsif mode == 3
+  # CPU utilization of system processes
+  perf_system = (100.to_f / cpu_time.to_f) * cpu_system.to_f
+  printPerfData(perf_system, warning, critical, "System")
+end
